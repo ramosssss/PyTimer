98811ddb92934456ad59a06e6ac2cdcacae50022
Martin Doege <mdoege@compuserve.com>
2017-01-22 14:35:19 +0100

    first commit

---
+++ pytimer.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+"""
+  Tkinter Python egg timer
+
+	Enter the number of minutes into the text entry and click Start
+"""
+
+import sys, time
+from tkinter import *
+import play_wav
+
+wav = "Question.wav"
+sound = play_wav.Sound()
+run = False
+curtime = 10
+altime = time.time() + 1e8
+
+def play_alarm():
+	sound.playFile(wav)
+	master.after(1200, play_alarm)
+
+def start():
+	global run, curtime, altime
+
+	if run and curtime < 1:
+		sys.exit(0)
+
+	if not run:
+		run = True
+		but.config(background = "green")
+		try:
+			mins = int(tt.get())
+		except:
+			mins = 1
+		altime = 60 * mins + time.time()
+		tt.config(state = "disabled")
+
+master = Tk()
+
+f1 = Frame(master)
+f1.pack(side = TOP, fill = Y)
+f2 = Frame(master)
+f2.pack(side = TOP, fill = Y)
+f3 = Frame(master)
+f3.pack(side = BOTTOM, fill = Y)
+
+tt = Entry(f2, width = 6)
+tt.pack(side = LEFT)
+tt.delete(0, END)
+
+but = Button(f1, text = "  Start  ", command = start)
+but.pack(side = LEFT)
+
+master.title("PyTimer")
+
+def showtime():
+	global curtime
+
+	curtime = max(0, int(altime - time.time()))
+
+	if run:
+		tt.config(state = "normal")
+		tt.delete(0, END)
+		tt.insert(0, "%u:%02u" % (curtime // 60, curtime % 60))
+		if curtime < 30:
+			but.config(background = "yellow")
+		if curtime < 2:
+			but.config(background = "red", text = "   Quit   ")
+		tt.config(state="disabled")
+	if curtime > 0:
+		master.after(1000, showtime)
+	else:
+		play_alarm()
+
+master.after(1000, showtime)
+
+mainloop()
+
